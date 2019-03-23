14e494c8c47e41eb7dc2d42e8587bc363d3e6ba3
Elvira6 <41555125+Elvira6@users.noreply.github.com>
2019-03-23 07:36:42 -0600

    Create StealLabel.java

---
+++ Demo3/src/model/StealLabel.java
@@ -0,0 +1,46 @@
+package model;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.text.Font;
+
+public class StealLabel  extends Label {
+	
+	public final static String FONT_PATH = "src/model/resources/kenvector_future_thin.ttf";
+	
+	public final static String BACKGROUND_IMAGE = "view/resources/blue_button13.png";
+	
+	public StealLabel(String text) {
+		
+		setPrefWidth(280);
+		setPrefHeight(49);
+		setText(text);
+		setWrapText(true);
+		setLabelFont();
+		setAlignment(Pos.CENTER);
+		
+		BackgroundImage backgroundImage = new BackgroundImage(new Image(BACKGROUND_IMAGE, 400/*width */, 50/*height*/, false, true), BackgroundRepeat.NO_REPEAT,
+				BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+		setBackground(new Background(backgroundImage));
+	}
+	
+	private void setLabelFont() {
+		
+		try {
+			setFont(Font.loadFont(new FileInputStream(new File(FONT_PATH)), 16));
+		}catch (FileNotFoundException e) {
+			setFont(Font.font("Verdana", 23));
+		}
+	}
+
+}
