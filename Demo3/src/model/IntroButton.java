58426b3917df0c5cb6e0c3b2c00243393bfeea29
Elvira6 <41555125+Elvira6@users.noreply.github.com>
2019-03-23 07:36:00 -0600

    Create IntroButton.java

---
+++ Demo3/src/model/IntroButton.java
@@ -0,0 +1,90 @@
+package model;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+import javafx.event.EventHandler;
+import javafx.event.Event;
+import javafx.scene.control.Button;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.text.Font;
+
+
+public class IntroButton extends Button {
+
+	
+	private final String FONT_PATH = "src/model/resources/kenvector_future_thin.ttf";
+	private final String BUTTON_PRESSED_STYLE = "-fx-background-color: transparent; -fx-background-image: url('/model/resources/blue_button00.png');";
+	private final String BUTTON_FREE_STYLE = "-fx-background-color: transparent; -fx-background-image: url('/model/resources/blue_button02.png');";
+	
+	public IntroButton(String text) {
+		setText(text);
+		setButtonFont();
+		setPrefWidth(190); //image width
+		setPrefHeight(33); //image height
+		setStyle(BUTTON_FREE_STYLE);
+		initializeButtonListeners();
+		
+	}
+	
+	private void setButtonFont() {
+		
+		try {
+			setFont(Font.loadFont(new FileInputStream(FONT_PATH), 23));
+		}catch (FileNotFoundException e) {
+			setFont(Font.font("Verdana", 23));
+			}
+	}
+	
+	private void setButtonPressedStyle() {
+		setStyle(BUTTON_PRESSED_STYLE);
+		setPrefHeight(45);
+		setLayoutY(getLayoutY() + 4);
+	}
+	
+	private void setButtonReleasedStyle() {
+		setStyle(BUTTON_FREE_STYLE);
+		setPrefHeight(49);
+		setLayoutY(getLayoutY()-4);
+	}
+	
+	private void initializeButtonListeners() {
+		
+		setOnMousePressed(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if(event.getButton().equals(MouseButton.PRIMARY)) {
+					setButtonPressedStyle();
+				}
+			}
+		});
+		
+		setOnMouseReleased(new EventHandler<MouseEvent>() {
+			@Override
+			public void handle(MouseEvent event) {
+				if (event.getButton().equals(MouseButton.PRIMARY)) {
+					setButtonReleasedStyle();
+				}
+			}
+		});
+		
+		setOnMouseEntered(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent event) {
+				setEffect(new DropShadow());
+			}
+		});
+		
+		setOnMouseExited(new EventHandler<MouseEvent>() {
+			
+			@Override
+			public void handle(MouseEvent event) {
+				setEffect(null);
+				
+			}
+		});
+	}
+	}
