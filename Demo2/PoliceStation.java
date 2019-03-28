9a0e7b894ec87657e51812e388364bada86fb044
teresaan <47434654+teresaan@users.noreply.github.com>
2019-03-27 20:19:49 -0600

    Updated Story Line

--- Demo2/PoliceStation.java
+++ Demo2/PoliceStation.java
@@ -24,20 +24,22 @@ public class PoliceStation {
 		
 		VBox root = new VBox();
 		Label storyLab = new Label("Your supervisor hands you a file containing information about a recent murder case. \n" +
-								"On January 31, 1978 at 0210 hrs, the local disco club was evacuated after the club bouncer \n" + 
-								"discovered an unidentified body in the corner of the dance floor. Cause of death was \n" + 
-								"determined to be blunt force trauma to the head. The bouncer states the DJ that night called \n" +
-								"him to break up a fight between two people. However, after arriving to the dance floor, the \n" +
-								"bouncer found only an unidentified victim on the floor. \n" +
-								"\n" +
-								"The DJ that night states he witnessed a fight between two people on the dance floor, but has \n" +
-								"not yet given a final statement to the authorities. \n" +
-								"\n" + 
-								"Reports identified a list of possible suspects, but more evidence is required for prosecution. \n" +
-								"\n" +
-								"It is your duty as a detective to determine WHO the murderer is and HOW they murdered the \n" +
-								"victim, and bring justice to us all or else the dance club will never re-open and disco will die.");
-        storyLab.setFont(Font.font("Times New Roman", 24)); 
+								   "It reads: \n" +
+								   "\n" +
+								   "'On January 31, 1978 at 0210 hrs, the local disco club was evacuated after the club bouncer \n" + 
+								   "discovered an unidentified body in the corner of the dance floor. Cause of death was \n" + 
+								   "determined to be blunt force trauma to the head. The bouncer states the DJ that night called \n" +
+								   "him to break up a fight between two people. However, after arriving to the dance floor, the \n" +
+								   "bouncer found only an unidentified victim on the floor. \n" +
+								   "\n" +
+								   "The DJ that night states he witnessed a fight between two people on the dance floor, but has \n" +
+								   "not yet given a final statement to the authorities. \n" +
+								   "\n" + 
+								   "Reports identified a list of possible suspects, but more evidence is required for prosecution.' \n" +
+								   "\n" +
+								   "It is your duty as a detective to determine WHO the murderer is and HOW they murdered the \n" +
+								   "victim, and bring justice to us all or else the dance club will never re-open and disco will die.");
+        storyLab.setFont(Font.font("Times New Roman", 24));  
         root.getChildren().add(storyLab);
 
 		Button LocA = new Button("Location A");
@@ -69,7 +71,5 @@ public class PoliceStation {
 		Scene application = new Scene(root, 847, 600);
 		primaryStage.setScene(application);
 		primaryStage.show();
-		primaryStage.showAndWait();
-		
 	}
 }
