3ad7364fd5314da023224f52d4023a4c132aa86b
abisubu18 <47370590+abisubu18@users.noreply.github.com>
2019-04-09 15:19:09 -0600

    Update LocationAView.java

--- Demo 3/Part2Demo3GUI/view/LocationAView.java
+++ Demo 3/Part2Demo3GUI/view/LocationAView.java
@@ -1,34 +1,51 @@
+package view;
 import javafx.stage.*;
 import javafx.scene.*;
 import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.geometry.*;
+import hangman.Hangman;
 
 import javafx.scene.shape.*;
 import javafx.scene.image.*;
 import javafx.scene.text.*;
 import javafx.stage.Stage;
+import model.StealLabel;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.control.*; 
 import javafx.geometry.*; 
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import model.TalkStealButton;
+import tictactoegui.TicTacToeGUI;
+public class LocationAView {
 
-/*
- *Class displays Location A (aka the Witness House). Scene 1 displays the doors 
- */
-public class LocationAGUI {
-
-    //Create static variables for all scenes 
+    /*
+     *Create static variables for all scenes. Scene 1 displays the door choice scene and scene
+     *4 displays the witness that allows you to recieve the next clue after playing a game of hangman.
+     *Initializes variable wondow as the stage and initializes an instance of PoliceStation 
+     */
     
-	protected static Scene scene1, scene4;
-    protected static Stage window;
+	public static Scene scene1, scene4;
+	private static Stage window;
+	public static PoliceStation ps;
 
-	public static Stage getStage() {
+	/**
+	 * Will get the window and return it
+	 * 
+	 * @param the current Stage specified 
+	 * @return the Stage called window
+	 */ 
+	public Stage getStage() {
         return window;
     }
-	
+	/*
+	*Will take the specific button specified and change it's shape into an arrow
+	* 
+	* @param the button for arrows pointing to the doors and sets their shapes as arrows
+	* @return no return
+	*/
 	public static void setPolygon(Button arrow){
 		Polygon polygon = new Polygon();
         polygon.getPoints().addAll(new Double[]{
@@ -44,49 +61,99 @@ public class LocationAGUI {
 		
 	}
 	
-			
+	/*
+	*Will take the specific button specified and change it's shape into an arrow
+	* 
+	* @param the button for arrows pointing to the doors and sets their shapes as arrows
+	* @return no return
+	*/		
 	public static void display() {
 		
+		Stage window = new Stage();
+	    window.initModality(Modality.APPLICATION_MODAL);
+	    window.setTitle("Location A");
+		
+	    
+	    /*
+	     * TalkStealButton riddleButton = new TalkStealButton("Play");
+		riddleButton.setLayoutX(100);
+		riddleButton.setLayoutY(200);
+		root4.getChildren().add(riddleButton);
+	     */
+	    
+	    
+	    
+	    
 		//block scene
-		Image blockImage = new Image("file:brickwall.png");
+		Image blockImage = new Image("view/resources/brickwall.png");
 		ImageView blockView = new ImageView(blockImage);
-		GridPane blockPane = new GridPane();
-		blockPane.setMaxSize(847, 600);
+		Group blockPane = new Group();
+		//blockPane.setMaxSize(800, 600);
 		blockPane.getChildren().add(blockView);
 		Scene block = new Scene(blockPane);
 		
+		/*
+		TalkStealButton backButton = new TalkStealButton("Go Back");
+		backButton.setLayoutX(50);
+		backButton.setLayoutY(420);
+		blockPane.getChildren().add(backButton);
+		
+		
+		backButton.setOnAction( e -> window.setScene(scene1));
+       */
 		
 		
 		
-	
 		
-        Stage window = new Stage();
-        window.initModality(Modality.APPLICATION_MODAL);
-        window.setTitle("locA");
-        
-      
-
+		
         //Background
-		Image background1 = new Image("file:doors2.jpg");
+		Image background1 = new Image("view/resources/doors2.jpg");
 		ImageView mv1 = new ImageView(background1);
-		GridPane root = new GridPane();
-		root.setMaxSize(847, 600);
+		Group root = new Group();
+		root.prefHeight(600);
+		root.prefWidth(800);
 		root.getChildren().add(mv1);
 		
+		/*
+		TalkStealButton backA = new TalkStealButton("Go Back");
+		backA.setLayoutX(50);
+		backA.setLayoutY(500);
+		root.getChildren().add(backA);
+		
+		
+		
+		
+		backA.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				ps.display1();
+			
+			}
+			});
+			
+		*/	
+		
+		
+
 		
 		//Intro Label 
 		HBox introloc = new HBox();
-		introloc.setAlignment(Pos.BOTTOM_CENTER);
 		Label intro = new Label("You have arrived at the witness's house, the front door is open, but you must navigate through the "+
 								"house to find the witness. Keep your eyes peeled, you never know what dangers may lurk!" );
-		System.out.println("You have arrived at the witness's house, the front door is open, but you must navigate through the "+
-								"house to find the witness. Keep your eyes peeled, you never know what dangers may lurk!");
+		intro.setLayoutX(100);
+		intro.setLayoutY(300);
 		intro.setTextFill(Color.web("#FFFFFF"));
 		intro.setFont(new Font("Times New Roman", 18));
-		intro.setTextAlignment(TextAlignment.CENTER);
 		intro.setWrapText(true);			
 		root.getChildren().add(introloc);
 		introloc.getChildren().add(intro);
+		
+		//Go Back button 
+		HBox gback = new HBox();
+		gback.setAlignment(Pos.TOP_LEFT);
+		TalkStealButton gbutton = new TalkStealButton("Go Back");
+		root.getChildren().add(gback);
+		gback.getChildren().add(gbutton);
 		
 		//Arrows
 		HBox arrows1  = new HBox(250);
@@ -94,11 +161,11 @@ public class LocationAGUI {
 		root.getChildren().add(arrows1);
 	
 		Button arrowA1= new Button("A");
-		arrowA1.setOnAction(e -> window.setScene(block));
+		arrowA1.setOnAction(e -> window.setScene(scene4));
 		setPolygon(arrowA1);
 		
 		Button arrowB1 = new Button("B");
-		arrowB1.setOnAction(e -> window.setScene(scene4));
+		arrowB1.setOnAction(e -> window.setScene(block));
 		setPolygon(arrowB1);
 		
 		Button arrowC1 = new Button("C");
@@ -106,7 +173,7 @@ public class LocationAGUI {
 		arrowC1.setOnAction(new EventHandler<ActionEvent>() {
 			@Override 
 			public void handle(ActionEvent attack) {
-				Scene attackerScene = attackGUI.getAttackScene();
+				Scene attackerScene = RPS.getAttackScene();
 				window.setScene(attackerScene);
 				//attackTerm.doattackTerm();
 			}
@@ -117,15 +184,67 @@ public class LocationAGUI {
 		arrows1.getChildren().add(arrowB1);
 		arrows1.getChildren().add(arrowC1);
 		
-		scene1 = new Scene(root);
 		
 		
 		
 		//Background4
-		Image background4 = new Image("file:witness.jpg");
+		Image background4 = new Image("view/resources/witness.jpg");
 		ImageView mv4 = new ImageView(background4);
-		GridPane root4 = new GridPane();
+		Group root4 = new Group();
+		Label lab = new Label("Help a witness to guess a word and get a new clue.");
+		lab.setTextFill(Color.web("#FFFFFF"));
+		lab.setFont(Font.font("Times New Roman", 24));
+		
 		root4.getChildren().add(mv4);
+		root4.getChildren().add(lab);
+		
+		TalkStealButton riddleButton = new TalkStealButton("Play");
+		riddleButton.setLayoutX(100);
+		riddleButton.setLayoutY(200);
+		root4.getChildren().add(riddleButton);
+
+		
+		
+		
+		riddleButton.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
+			public void handle(ActionEvent event) {
+				Hangman hangman = new Hangman();
+				hangman.start(new Stage());
+			}
+		});
+		
+		TalkStealButton backfromA = new TalkStealButton("Go Back");
+		backfromA.setLayoutX(100);
+		backfromA.setLayoutY(250);
+		root4.getChildren().add(backfromA);
+		
+		backfromA.setOnAction( e -> window.setScene(scene1));
+		
+		
+		
+		
+		
+		
+		/*
+		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+			public void handle(ActionEvent event) {
+				riddleButton.setOnAction(e -> {
+					Hangman hangman = new Hangman();
+					hangman.start(new Stage());
+				
+			});
+			
+			}
+		};
+		*/
+		
+		/*
+		riddleButton.setOnAction(e -> {
+			Riddle2.display();
+		});
+		*/
+		
 		
 		
 		scene4 = new Scene(root4);
@@ -133,10 +252,11 @@ public class LocationAGUI {
 		
 		
 		
-		//scene1 = new Scene(root);
+		scene1 = new Scene(root, 800, 600);
 		window.setScene(scene1);
 		window.showAndWait();
 
     }
 
 }
+
