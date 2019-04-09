eb842a2e90949bc57715ff973959fddb8e34f452
teresaan <47434654+teresaan@users.noreply.github.com>
2019-04-09 15:30:48 -0600

    Create TicTacToeTest.java

---
+++ Demo 3/Part2Demo3GUI/tictactoegui/TicTacToeTest.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TicTacToeTest extends TicTacToeGame {
+  @Test
+	public void testAddRating_upperbound() {	
+		TicTacToe t = new TicTacToe();
+		assertEquals("Created credit history with invalid rating (greater than 5)", 0, ch.getRatings().size());
+	}
+
+}
