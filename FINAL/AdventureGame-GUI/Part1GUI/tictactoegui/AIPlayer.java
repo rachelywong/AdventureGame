088376d895bfce25648246184385b030bd517851
abisubu18 <47370590+abisubu18@users.noreply.github.com>
2019-04-12 10:42:57 -0600

    Update AIPlayer.java

--- FINAL/AdventureGame-GUI/Part1GUI/tictactoegui/AIPlayer.java
+++ FINAL/AdventureGame-GUI/Part1GUI/tictactoegui/AIPlayer.java
@@ -1,12 +1,27 @@
+/**
+* @author Team 7
+* This class creates the AIPlayer 
+*code from Dr. Nathaly Verwaal (please see reference file)
+*/
 package tictactoegui;
 
 public class AIPlayer {
     private char token = 'x';
      
+    /**
+	* Constructer for placing token 
+	* @param char 
+	 *@return void 
+	*/
     public AIPlayer(char aToken) {
         setToken(aToken);
     }
-     
+    
+    /**
+	* Setter for token 
+	* @param char
+	 *@return void
+	*/
     public void setToken(char aToken){
         if (aToken == 'x' || aToken == 'o') {
             token = aToken;
@@ -16,7 +31,11 @@ public class AIPlayer {
     public char getToken() {
         return token;
     }
-     
+    /**
+	* Getter checks movement for placement 
+	* @param board
+	 *@return Move
+	*/
     public Move getMove(TicTacToeBoard board){
         int row = 0;
         int column = 0;
