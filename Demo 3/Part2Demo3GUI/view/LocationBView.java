24b920362187b91f9a2f9aa4551dbc0b0d1955bf
Elvira6 <41555125+Elvira6@users.noreply.github.com>
2019-04-11 22:06:10 -0600

    Update LocationBView.java

--- Demo 3/Part2Demo3GUI/view/LocationBView.java
+++ Demo 3/Part2Demo3GUI/view/LocationBView.java
@@ -1,136 +1,134 @@
-/**
- * @author Team 7
- */
 
-/**
- * Class represents Location B graphic user interface for the game.
- * <p>
- * GUI is created using multiple images, labels, and buttons with Action events
- * to help guide user through this part of the game.
- */
 package view;
+import javafx.stage.*;
+import javafx.scene.*;
+import javafx.scene.layout.*;
+import javafx.scene.control.*;
+import javafx.geometry.*;
+import hangman.Hangman;
 
 import javafx.scene.shape.*;
 import javafx.scene.image.*;
 import javafx.scene.text.*;
-import javafx.scene.*;
-import javafx.stage.*;
-import model.TalkStealButton;
-import tictactoegui.TicTacToeGUI;
+import javafx.stage.Stage;
+import model.StealLabel;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.control.*; 
 import javafx.geometry.*; 
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
-import javafx.application.Application;
-import javafx.animation.AnimationTimer;
-import javafx.scene.Group;
- 
-/////CONNECT POLICE STATION
-public class LocationBView {
-	
-	/**
-     	 * Create static variables for all scenes.
-    	 */
-	static Scene crimeScene, weaponScene, framedScene;
-	
+import model.TalkStealButton;
+public class LocationB {
+
+    
+	public static Scene crimeScene, weaponScene, framedScene;
+	private static Stage window;
 	public static PoliceStation ps;
 
+	 
+	public Stage getStage() {
+        return window;
+    }
+	
+
+	/*
+	*Will take the specific button specified and change it's shape into an arrow
+	* 
+	* @param the button for arrows pointing to the doors and sets their shapes as arrows
+	* @return no return
+	*/		
+	
+	
 	public static void display() {
-        Stage primaryStage = new Stage();
-        primaryStage.initModality(Modality.APPLICATION_MODAL);
-        
-    
-        
-        /*
-        Image background1 = new Image("view/resources/crimescene.png");
-      	ImageView mv1 = new ImageView(background1);
-      	mv1.setFitHeight(600);
-      	mv1.setFitWidth(800);
-      	*/
-      	
+		
+		Stage window = new Stage();
+	    window.initModality(Modality.APPLICATION_MODAL);
+	    window.setTitle("Location B");
+		
+	
+        //Background
+	    
+		Image background1 = new Image("view/resources/crime.jpg");
+		ImageView mv1 = new ImageView(background1);
+		GridPane root = new GridPane();
+		root.setMaxSize(800, 600);
+		root.prefHeight(600);
+		root.prefWidth(800);
+		root.getChildren().add(mv1);
 
-
-        /*
-        //crimeScene Background
-      	Image background1 = new Image("/view/resources/crimescene.png");
-      	ImageView mv1 = new ImageView(background1);
-      	GridPane root = new GridPane();
-      	root.setMaxSize(800, 600);
-      	root.getChildren().add(mv1);
-      	*/
-      	
 		
-	//scene1 - crimeScene
-        Group root1 = new Group();
-        //root1.getChildren().add(mv1);
-        Label storyLab = new Label("You arrive at the crime scene and begin to look around the club for any clues.\n" +
-								   "The bouncer tells you that the DJ who was playing the night of the murder is \n" +
-								   "here if you would like to speak to him." + 
-								   "\n" +
-								   "Would you like to speak to the DJ, or examin the weapon first?");
-		//storyLab.setTextFill(Color.web("#FFFFFF"));				   
-        storyLab.setFont(Font.font("Times New Roman", 24));
-        root1.getChildren().add(storyLab);
-        
-        
-        /*
-        TalkStealButton backfromB = new TalkStealButton("Go Back");
-        backfromB.setLayoutX(50);
-        backfromB .setLayoutY(500);
-		root1.getChildren().add(backfromB);
+		//Intro Label 
+		HBox introloc = new HBox();
+		Label intro = new Label("You have arrived at the Crime scene" );
+		introloc.setAlignment(Pos.TOP_CENTER);
+		//intro.setLayoutX(100);
+		//intro.setLayoutY(300);
+		intro.setTextFill(Color.web("#FFFFFF"));
+		intro.setFont(new Font("Times New Roman", 18));
+		intro.setWrapText(true);			
+		root.getChildren().add(introloc);
+		introloc.getChildren().add(intro);
 		
 		
-		
-		
-		backfromB .setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				ps.display1();
-			
-			}
-			});
-			
-			*/
-        
-        
-        
-        
+		//Buttons
+		HBox butt = new HBox(250);
+		butt.setAlignment(Pos.CENTER);
+		root.getChildren().add(butt);
 		//DJButton
-        TalkStealButton DJButton = new TalkStealButton("Talk to DJ");
-        DJButton.setLayoutX(20);
+		TalkStealButton DJButton = new TalkStealButton("Talk to DJ");
+		//weaponButton
+	    DJButton.setLayoutX(20);
         DJButton.setLayoutY(190);
-        DJButton.setOnAction(new HandleButtonClick("DJ", storyLab));
-        root1.getChildren().add(DJButton);
+        //DJButton.setOnAction(new HandleButtonClick("DJ", storyLab));
+        root.getChildren().add(DJButton);
 		//directs to Riddle1 class
 		DJButton.setOnAction(e -> {
-			primaryStage.hide();
+			window.hide();
 			Riddle1.display();
 		});
-		//weaponButton
+		
 		TalkStealButton weaponButton = new TalkStealButton("Examine weapon");
 		weaponButton.setLayoutX(20);
 		weaponButton.setLayoutY(260);
-		weaponButton.setOnAction(new HandleButtonClick("Weapon", storyLab));
-        root1.getChildren().add(weaponButton);
-		//directs to scene3
-		weaponButton.setOnAction(e -> primaryStage.setScene(weaponScene));
+		//weaponButton.setOnAction(new HandleButtonClick("Weapon", storyLab));
+		root.getChildren().add(weaponButton);
+        //directs to scene3
 		
-	//scene3 - weaponScene
-		VBox root3 = new VBox();
-        Label choice3Label = new Label("Pick up weapon or take pic.");
-        choice3Label.setFont(Font.font("Times New Roman", 24));
-        root3.getChildren().add(choice3Label);
-		//pickUpButton 
-        TalkStealButton pickUpButton = new TalkStealButton("Pick up weapon");
-		pickUpButton.setOnAction(new HandleButtonClick("Pick up", choice3Label));
-		root3.getChildren().add(pickUpButton);
-		//directs to scene 4
-		pickUpButton.setOnAction(e -> primaryStage.setScene(framedScene));
-		//photoButton 
-		TalkStealButton photoButton = new TalkStealButton("Take picture of weapon");
-        photoButton.setOnAction(new HandleButtonClick("Puzzle", choice3Label));
+	    weaponButton.setOnAction(e -> window.setScene(weaponScene));
+	    //weaponButton.setOnAction(e -> window.setScene(block));
+		
+		
+	    butt.getChildren().add(DJButton);
+	    butt.getChildren().add(weaponButton);
+	    
+
+	  //scene3 - weaponScene
+	    Image background3 = new Image("view/resources/crime.jpg");
+	    ImageView mv3 = new ImageView(background3);
+	    Group root3 = new Group();
+	    Label lab = new Label("Pick up weapon or take a photo.");
+	    lab.setLayoutX(300);
+	    lab.setLayoutY(1);
+	    lab.setTextFill(Color.web("#FFFFFF"));
+		lab.setFont(Font.font("Times New Roman", 24));
+		
+		root3.getChildren().add(mv3);
+		root3.getChildren().add(lab);
+		
+		TalkStealButton pickUpButton = new TalkStealButton("Pick up weapon");
+		pickUpButton.setLayoutX(100);
+		pickUpButton.setLayoutY(200);
+  		pickUpButton.setOnAction(new HandleButtonClick("Pick up", lab));
+  		root3.getChildren().add(pickUpButton);
+  		
+  		pickUpButton.setOnAction(e -> window.setScene(framedScene));
+  		
+  		
+  		TalkStealButton photoButton = new TalkStealButton("Take picture of weapon");
+  		pickUpButton.setLayoutX(100);
+		pickUpButton.setLayoutY(250);
+        photoButton.setOnAction(new HandleButtonClick("Puzzle", lab));
         root3.getChildren().add(photoButton);
         photoButton.setOnAction(new EventHandler<ActionEvent>() {
 			
@@ -138,44 +136,47 @@ public class LocationBView {
 			public void handle(ActionEvent event) {
 					Puzzle puzzle = new Puzzle();
 					
-					puzzle.start(new Stage()); //chooseDetective
-					//obj.thefunctioname(args);
-					//Class2.main(args);
+					puzzle.start(new Stage()); 
+					
 				
 
 			}
 		});
-        
-        
+	    
+            
+
+	        //scene4 - framedScene
+            Image background4 = new Image("view/resources/crime.jpg");
+            ImageView mv4 = new ImageView(background4);
+	  		Group root4 = new Group();
+	          Label frameLabel = new Label("You've been framed! Your captain demands that you return to \n" +
+	  									 "the station immediately for questioning. You may continue the \n" +
+	  									 "case after.");
+	          frameLabel.setFont(Font.font("Times New Roman", 24));
+	          frameLabel.setTextFill(Color.web("#FFFFFF"));
+	          root4.getChildren().add(mv4);
+	          root4.getChildren().add(frameLabel);
+	  		//PS2Button
+	          TalkStealButton PS2Button = new TalkStealButton("Back to Police Station");
+	          PS2Button.setLayoutX(1);
+	          PS2Button.setLayoutY(150);
+	          PS2Button.setOnAction(new HandleButtonClick("PS", frameLabel));
+	          root4.getChildren().add(PS2Button);
+	  		//directs to PS
+	  		PS2Button.setOnAction(e -> {
+	  			window.hide();
+	  			//PoliceStation.PS();
+	  		});
+	   
 		
-	
-		
-		
-	    //scene4 - framedScene
-		VBox root4 = new VBox();
-        Label frameLabel = new Label("You've been framed! Your captain demands that you return to \n" +
-									 "the station immediately for questioning. You may continue the \n" +
-									 "case after.");
-        frameLabel.setFont(Font.font("Times New Roman", 24));
-        root4.getChildren().add(frameLabel);
-		//PS2Button
-        TalkStealButton PS2Button = new TalkStealButton("Back to Police Station");
-        PS2Button.setOnAction(new HandleButtonClick("PS", frameLabel));
-        root4.getChildren().add(PS2Button);
-		//directs to PS
-		PS2Button.setOnAction(e -> {
-			primaryStage.hide();
-			//PoliceStation.PS();
-		});
-		
-		
-		framedScene = new Scene(root4, 847, 600);
+	  	framedScene = new Scene(root4, 847, 600);
 		weaponScene = new Scene(root3, 847, 600);
-  		crimeScene = new Scene(root1, 847, 600);
-  		
-		primaryStage.setTitle("Location B");
-        primaryStage.setScene(crimeScene);
-        primaryStage.show();
-	}
+
+		crimeScene = new Scene(root, 800, 600);
+		window.setScene(crimeScene);
+		window.showAndWait();
+
+    }
+
 }
 
