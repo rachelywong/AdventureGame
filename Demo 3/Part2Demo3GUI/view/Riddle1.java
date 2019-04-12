2972a5da9d709cdea384f0ea9a1cf747efb0d01d
Elvira6 <41555125+Elvira6@users.noreply.github.com>
2019-04-12 01:15:05 -0600

    Update Riddle1.java

--- Demo 3/Part2Demo3GUI/view/Riddle1.java
+++ Demo 3/Part2Demo3GUI/view/Riddle1.java
@@ -1,10 +1,3 @@
-/**
- * @author Team7
- */
-
-/**
- * Class represents a riddle.
- */
 
 package view;
 
@@ -23,39 +16,48 @@ import javafx.event.ActionEvent;
 import javafx.application.Application;
 import javafx.animation.AnimationTimer;
 import javafx.scene.Group;
+import model.Clabel;
  
 /////CONNECT POLICE STATION
-public class Riddle1 {
+public class Riddle1 extends PoliceStation{
 
 	public static void display() {
         Stage primaryStage = new Stage();
         primaryStage.initModality(Modality.APPLICATION_MODAL);
         primaryStage.setTitle("Riddle1");
 
-	Group root = new Group();
+        
+        
+        Image backgroundR = new Image("view/resources/bouncer1.jpg");
+		ImageView mvR = new ImageView(backgroundR);
+		Group root = new Group();
 		
 	//Story Line
         Label storyLab = new Label("You find the DJ at the club and ask him to tell you what he saw that night." +
-				   "\n" +
-				   "DJ: 'If you want my witness account, you'll have to prove to me you're \n" +
-				   "a good detective. Solve the following riddle.' \n" +
-				   "\n" +
-			  	   "I can be cracked, I can be made. I can be told, I can be played.");
-        storyLab.setFont(Font.font("Times New Roman", 24));
+								   "\n" +
+								   "DJ: 'If you want my witness account, you'll have to prove to me you're \n" +
+								   "a good detective. Solve the following riddle.' \n" +
+								   "\n" +
+								   "I can be cracked, I can be made. I can be told, I can be played.");
+        storyLab.setLayoutX(240);
+        storyLab.setLayoutY(50);
+        storyLab.setFont(Font.font("Times New Roman", 15));
+        root.getChildren().add(mvR);
         root.getChildren().add(storyLab);
+        
 
 	//Labels
 		//promptLab
 		Label promptLab = new Label("What am I? (Press the submit button to enter guess)");
-		promptLab.setLayoutX(25);
+		promptLab.setLayoutX(240);
 		promptLab.setLayoutY(150);
-		promptLab.setFont(Font.font("Times New Roman", 20));
+		promptLab.setFont(Font.font("Times New Roman", 15));
 		root.getChildren().add(promptLab);
 		//resultLab
-		Label resultLab = new Label(" ");
-		resultLab.setLayoutX(225);
+		Clabel resultLab = new Clabel(" ");
+		resultLab.setLayoutX(410);
 		resultLab.setLayoutY(250);
-		resultLab.setFont(Font.font("Times New Roman", 20));
+		//resultLab.setFont(Font.font("Times New Roman", 20));
 		root.getChildren().add(resultLab);
 		//txtField
 		TextField txtField = new TextField("(2 words, first word is A)");
@@ -65,13 +67,16 @@ public class Riddle1 {
 
 	//Buttons
 		//PSButton
-		TalkStealButton PSButton = new TalkStealButton("Go back to PS");
+		TalkStealButton PSButton = new TalkStealButton("Back to Map");
         PSButton.setLayoutX(20);
         PSButton.setLayoutY(300);
 		root.getChildren().add(PSButton);
 		PSButton.setOnAction(new HandleButtonClick("PS", promptLab));
 		PSButton.setOnAction(e -> {
 			primaryStage.hide();
+			talkStage.show();
+			
+			
 			//PoliceStation.show();
 		});
 		//submitButton
@@ -84,19 +89,21 @@ public class Riddle1 {
 	    EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e1) {						
 				if (txtField.getText().toUpperCase().equals("A JOKE")) {
-					resultLab.setText("DJ: 'Nice job pal you got it correct! You can call \n "+
-							          "me Disco Joe then! Let me tell you, \n" +
-								      "I was there that night everything went down. To be honest,\n " +
-								      "I don't remember much. I \n" +
-								      "didnt notice the one dude hitting the other dude \n" +
-								      "because they were going at it to the \n" + 
-								      "beat of the jam that was playing. I thought it was some sort of \n" +
-								      "new dance move! I do remember \n" +
-								      "the kid who was doing most of the whacking had brown hair though.'\n" +
+					resultLab.setText("DJ: 'Nice job pal you got it correct! You can call \n" +
+							          "me Disco Joe then! Let me tell you, I was there    \n" +
+								      "that night everything went down. To be honest, I   \n" +
+							          "don't remember much. I didn't notice the one dude  \n" +
+								      "hitting the other dude because they were going at  \n" +
+								      "it to the beat of the jam that was playing. I      \n" + 
+								      "thought it was some sort of new dance move! I do   \n" +
+								      "remember the kid who was doing most of the         \n" +
+								      "whacking had brown hair though.'\n" +
 									  "\n" +
-									  "Return to the police station to report and continue.");
+									  "Return to the police station to report \n" +
+									  "and continue.");
 				} else {
 					resultLab.setText("Incorrect. Please try again.");
+					
 				}
 			}
 	    };		
