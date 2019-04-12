00757026b2f9fd8816b400f518d66af45f164b7a
abisubu18 <47370590+abisubu18@users.noreply.github.com>
2019-04-12 10:18:56 -0600

    Update Detective.java

--- FINAL/AdventureGame-GUI/Part1GUI/model/Detective.java
+++ FINAL/AdventureGame-GUI/Part1GUI/model/Detective.java
@@ -1,3 +1,7 @@
+/**
+* @author Team 7
+* This class imports images as two assets that (good and bad detectives) and allows for us to use them 
+*/
 package model;
 
 public enum Detective {
@@ -7,11 +11,20 @@ public enum Detective {
 	
 	private String urlDetective;
 	
+	/**
+	* method creates Detective instance with  urlDetective
+	* @param String
+	 *@return Detective 
+	*/
 	private Detective(String urlDetective) {
 		this.urlDetective = urlDetective;
 		}
 	
-	
+	/**
+	* getter for urlDetective 
+	* @param String
+	 *@return Detective
+	*/
 	public String getUrl() {
 		return this.urlDetective;
 	}
