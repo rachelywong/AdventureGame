836611dee3398f2769d721529ceb0678f8a8d367
Rachel Wong <45707735+rachelywong@users.noreply.github.com>
2019-04-12 07:35:01 -0600

    Update Dictionary.java

--- Demo 3/Part1Demo3GUI/hangman/Dictionary.java
+++ Demo 3/Part1Demo3GUI/hangman/Dictionary.java
@@ -1,8 +1,19 @@
 package hangman;
 
+/**
+* @author Team 7
+*/
 
+/**
+* This class generates the hiddenword used for the hangman game.
+*/
 public class Dictionary {
 
+	/**
+	 * Initializes and hiddenword as "DISCO"
+	 * @param String
+	 * @return String: word
+	 */
 	public String getHiddenWord() {
 		
 		String word = "DISCO";
