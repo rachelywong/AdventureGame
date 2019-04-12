1a666dfe31a7082c271933590fa01a9fd0a69504
Elvira6 <41555125+Elvira6@users.noreply.github.com>
2019-04-12 01:10:36 -0600

    Update LocationAView.java

--- Demo 3/Part2Demo3GUI/view/LocationAView.java
+++ Demo 3/Part2Demo3GUI/view/LocationAView.java
@@ -62,7 +62,7 @@ public class LocationAView {
 	}
 	
 	/*
-	*Will create a new Stage and display scene 1 (doors) on the stage. Background is a 
+	*Will take the specific button specified and change it's shape into an arrow
 	* 
 	* @param the button for arrows pointing to the doors and sets their shapes as arrows
 	* @return no return
@@ -71,26 +71,12 @@ public class LocationAView {
 		
 		Stage window = new Stage();
 	    window.initModality(Modality.APPLICATION_MODAL);
-	    window.setTitle("Location A");
-		
-	    
-	    /*
-	     * TalkStealButton riddleButton = new TalkStealButton("Play");
-		riddleButton.setLayoutX(100);
-		riddleButton.setLayoutY(200);
-		root4.getChildren().add(riddleButton);
-	     */
+	    window.setTitle("Witness House");
+	
 	    
 	    
 	    
-	    
-		
-		/*
-		 *Block scene
-		 *
-		 *Creating scene for the blocked pathway using ImageView() method. 
-		 *TalkStealButtons are used display formatted Play buttons. 
-		 */
+		//block scene
 		Image blockImage = new Image("view/resources/brickwall.png");
 		ImageView blockView = new ImageView(blockImage);
 		Group blockPane = new Group();
@@ -98,99 +84,53 @@ public class LocationAView {
 		blockPane.getChildren().add(blockView);
 		Scene block = new Scene(blockPane);
 		
-		/*
-		TalkStealButton backButton = new TalkStealButton("Go Back");
+		
+		TalkStealButton backButton = new TalkStealButton("Back to Witness House");
 		backButton.setLayoutX(50);
 		backButton.setLayoutY(420);
 		blockPane.getChildren().add(backButton);
 		
 		
 		backButton.setOnAction( e -> window.setScene(scene1));
-       */
+       
 		
 		
 		
-
-		/*
-		 *Background
-		 *
-		 *Initial backdrop of beginning of Location A. 
-		 *Using metho Imageview()
-		 */
 		
+		
+        //Background
 		Image background1 = new Image("view/resources/doors2.jpg");
 		ImageView mv1 = new ImageView(background1);
-		Group root = new Group();
-		root.prefHeight(600);
+		GridPane root = new GridPane();
+		root.setMaxSize(800, 600);
 		root.prefWidth(800);
+		root.prefHeight(600);
 		root.getChildren().add(mv1);
 		
-		/*
-		TalkStealButton backA = new TalkStealButton("Go Back");
-		backA.setLayoutX(50);
-		backA.setLayoutY(500);
-		root.getChildren().add(backA);
-		
-		
-		
-		
-		backA.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent event) {
-				ps.display1();
-			
-			}
-			});
-			
-		*/	
 		
 		
 
-		/*
-		 *Intro Label 
-		 *
-		 *Creates HBox introloc which displays the initial instructions
-		 *Creates a label to introduce the initial challenge of finding the correct door that has the witness.
-		 *Formats label and adds to HBox introloc
-		 */
 		
+		//Intro Label 
 		HBox introloc = new HBox();
+		Label intro = new Label("You have arrived at the witness's house, the front door is open, but you must   \n" +
+								"navigate through the house to find the witness. Keep your eyes peeled, you  \n" +
+								"never know what dangers may lurk!" );
 		introloc.setAlignment(Pos.BOTTOM_CENTER);
-		Label intro = new Label("You have arrived at the witness's house, the front door is open, but you must navigate through the "+
-								"house to find the witness. Keep your eyes peeled, you never know what dangers may lurk!" );
-		System.out.println("You have arrived at the witness's house, the front door is open, but you must navigate through the "+
-								"house to find the witness. Keep your eyes peeled, you never know what dangers may lurk!");
+		//intro.setLayoutX(100);
+		//intro.setLayoutY(300);
 		intro.setTextFill(Color.web("#FFFFFF"));
-		intro.setFont(new Font("Times New Roman", 18));
-		intro.setTextAlignment(TextAlignment.CENTER);
+		intro.setFont(new Font("Times New Roman", 23));
 		intro.setWrapText(true);			
 		root.getChildren().add(introloc);
 		introloc.getChildren().add(intro);
 		
-		
-		/*
-		 *Go Back button 
-		 *
-		 *Creates HBox gback which allows user to return to the police station.
-		 *Creates new TalkStealButton that displays "Go Back". 
-		 *Button is added to Hbox gback which is added to the root
-		 */
-		HBox gback = new HBox();
-		gback.setAlignment(Pos.TOP_LEFT);
-		TalkStealButton gbutton = new TalkStealButton("Go Back");
-		root.getChildren().add(gback);
-		gback.getChildren().add(gbutton);
+	
+	
 		
 		
-		/*
-		 *Arrows
-		 *
-		 *Creates HBox arrows1 at size 250. HBox arrows1 is added to the root. 
-		 *3 new buttons are created, formatted using the method setPolygon(). 
-		 *Arrow buttons either lead to a blocked wall, a Rock Paper Scissors mini game or 
-		 *to the witness. 
-		 *Buttons are added to HBox arrows1. 
-		 */
+		
+		//Arrows
 		HBox arrows1  = new HBox(250);
 		arrows1.setAlignment(Pos.CENTER);
 		root.getChildren().add(arrows1);
@@ -198,6 +138,7 @@ public class LocationAView {
 		Button arrowA1= new Button("A");
 		arrowA1.setOnAction(e -> window.setScene(scene4));
 		setPolygon(arrowA1);
+		
 		
 		Button arrowB1 = new Button("B");
 		arrowB1.setOnAction(e -> window.setScene(block));
@@ -210,7 +151,7 @@ public class LocationAView {
 			public void handle(ActionEvent attack) {
 				Scene attackerScene = RPS.getAttackScene();
 				window.setScene(attackerScene);
-				
+				//attackTerm.doattackTerm();
 			}
 		});
 		
@@ -220,13 +161,9 @@ public class LocationAView {
 		arrows1.getChildren().add(arrowC1);
 		
 		
-	
-		/*
-		 *Background4
-		 *
-		 *Creating scene for the witness that calls the Hangman mini-game. 
-		 *TalkStealButtons are used display formatted Play buttons. 
-		 */
+		
+		
+		//Background4
 		Image background4 = new Image("view/resources/witness.jpg");
 		ImageView mv4 = new ImageView(background4);
 		Group root4 = new Group();
@@ -236,6 +173,7 @@ public class LocationAView {
 		
 		root4.getChildren().add(mv4);
 		root4.getChildren().add(lab);
+		
 		
 		TalkStealButton riddleButton = new TalkStealButton("Play");
 		riddleButton.setLayoutX(100);
@@ -253,36 +191,21 @@ public class LocationAView {
 			}
 		});
 		
-		TalkStealButton backfromA = new TalkStealButton("Go Back");
+		
+		TalkStealButton backfromA = new TalkStealButton("Back to Map");
 		backfromA.setLayoutX(100);
 		backfromA.setLayoutY(250);
 		root4.getChildren().add(backfromA);
 		
-		backfromA.setOnAction( e -> window.setScene(scene1));
-		
-		
-		
-		
-		
-		
-		/*
-		EventHandler<ActionEvent> event = new EventHandler<ActionEvent>() {
+		backfromA.setOnAction(new EventHandler<ActionEvent>() {
+			@Override
 			public void handle(ActionEvent event) {
-				riddleButton.setOnAction(e -> {
-					Hangman hangman = new Hangman();
-					hangman.start(new Stage());
-				
-			});
+				ps.display1();
+				window.hide();
 			
 			}
-		};
-		*/
+			});
 		
-		/*
-		riddleButton.setOnAction(e -> {
-			Riddle2.display();
-		});
-		*/
 		
 		
 		
