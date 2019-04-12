068acbd2e9187487dafa7b9b9fcbf8db1e7158c7
Elvira6 <41555125+Elvira6@users.noreply.github.com>
2019-04-12 01:12:46 -0600

    Update LocationCView.java

--- Demo 3/Part2Demo3GUI/view/LocationCView.java
+++ Demo 3/Part2Demo3GUI/view/LocationCView.java
@@ -12,6 +12,7 @@ import model.TalkStealButton;
 import javafx.geometry.*; 
 import javafx.event.EventHandler;
 import javafx.event.ActionEvent;
+import model.Clabel;
 
 import javafx.application.Application;
 import javafx.animation.AnimationTimer;
@@ -21,35 +22,38 @@ public class LocationCView extends PoliceStation {
 	public static void display() {
         Stage primaryStage = new Stage();
         primaryStage.initModality(Modality.APPLICATION_MODAL);
-        primaryStage.setTitle("Location C");
+        primaryStage.setTitle("Local Gym");
 
-		Group root = new Group();
-		Scene scene = new Scene(root, 847, 600);
 		
-		Label storyLab = new Label("You arrive at the local gym, thinking the suspect may work out here since \n" +
-								   "they  must be pretty strong to cause blunt force trauma. The local workers \n" +
-								   "at the gym give you a master locker key so you can open any locker you please! \n" +
-								   "You walk into the locker room to find over a billion lockers. Which locker \n" +
-								   "do you want to open?");
+		
+        Image backgroundC = new Image("view/resources/lockers.png");
+		ImageView mvC = new ImageView(backgroundC);
+		Group root = new Group();
+		Label storyLab = new Label("You arrive at the local gym, thinking the suspect may work out here since they must \n" +
+								   "be pretty strong to cause blunt force trauma. The local workers at the gym give you \n" +
+								   "at the gym give you a master locker key so you can open any locker you please! You walk \n" +
+								   "into the locker room to find over a billion lockers. Which locker do you want to open?");
+		storyLab.setLayoutX(10);
+		storyLab.setLayoutY(10);
 		storyLab.setFont(Font.font("Times New Roman", 21));
+		root.getChildren().add(mvC);
 		root.getChildren().add(storyLab);
 
 		Label promptLab = new Label("Guess:");
-		promptLab.setLayoutX(20);
-		promptLab.setLayoutY(140);
+		promptLab.setLayoutX(10);
+		promptLab.setLayoutY(115);
 
-		Label resultLab = new Label("Result");
-		resultLab.setLayoutX(20);
-		resultLab.setLayoutY(175);
+		Clabel resultLab = new Clabel("Result:");
+		resultLab.setLayoutX(300);
+		resultLab.setLayoutY(115);
 
 		TextField txtField = new TextField("(4 digits)");
-		txtField.setLayoutX(70);
-		txtField.setLayoutY(135);
+		txtField.setLayoutX(60);
+		txtField.setLayoutY(110);
 
 		// create a button
-		TalkStealButton PSButton = new TalkStealButton("Go back to PS");
+		TalkStealButton PSButton = new TalkStealButton("Back to Map");
 		TalkStealButton submitButton = new TalkStealButton("Submit");
-		//TalkStealButton LocAButton = new TalkStealButton("Location A");
 		TalkStealButton hintButton = new TalkStealButton("Hint");
 
         PSButton.setLayoutX(20);
@@ -57,7 +61,7 @@ public class LocationCView extends PoliceStation {
         PSButton.setOnAction( e ->{
         	primaryStage.hide();
         	talkStage.show();
-        	//PoliceStation.PS();
+        	
         });
         submitButton.setLayoutX(20);
         submitButton.setLayoutY(230);
@@ -89,14 +93,16 @@ public class LocationCView extends PoliceStation {
 	    EventHandler<ActionEvent> event2 = new EventHandler<ActionEvent>() {
 			public void handle(ActionEvent e2) {						
 				if (txtField.getText().toUpperCase().equals("1978")) {
-					resultLab.setText("You open a locker to find a diary with a bookmarked page \n" +
-					"dated as January 31st. Reading the page it says: \n" +
-					"'I just murdered someone.' \n" + 
-					"You look through the rest of the locker to find a box of cereal, some \n" +
-					"clean white socks, and a wrench.");
+					resultLab.setText("      You open a locker to find a diary with a  \n" +
+					"     bookmarked page dated as January 31st.   \n" +
+					"     Reading the page it says: 'I just murdered   \n" + 
+					"     someone.' You look through the rest of the  \n" +
+					"     locker to find a box of cereal, someclean \n" +
+					"     white socks, and a wrench.");
 				} else {
-					resultLab.setText("Empty locker. Please try again, or press the hint button \n" +
-					"to solve a riddle for a little hint to help you.");
+					resultLab.setText("   Empty locker. Please try again, or press the   \n" +
+					"   hint button to solve a riddle for a little \n" +
+					"   hint to help you.");
 				}
 			}
 	    };	
@@ -126,6 +132,7 @@ public class LocationCView extends PoliceStation {
 		//LocAButton.setOnAction(event3);
 		//hintButton.setOnAction(event4);
 
+	    Scene scene = new Scene(root, 847, 600);
 		root.getChildren().addAll(promptLab, txtField, resultLab, PSButton, submitButton, hintButton);
 		primaryStage.setScene(scene);
 		primaryStage.show();
